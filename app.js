43684e151a29accf0c8e6a39a388efa6223d8778
Bruno Leal <bruno_nleal@hotmail.com>
2024-08-06 14:35:54 -0300

    fsdfgsgad

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post("/login", (req, res) => {
 
 app.get("/home", (req, res) => {
   try {
-    res.render("home", { content: JSON.stringify(req) });
+    res.render("home", { content: JSON.stringify(req.query.code) });
     // res.render("home", { content: "API Response." });
   } catch (err) {
     console.log("Algo deu errado =/", err);
