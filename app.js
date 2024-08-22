9fb600ded6d579870d1566ae2ed0967053ecc09e
Bruno Leal <bruno_nleal@hotmail.com>
2024-08-22 16:52:13 -0300

    dasd

--- app.js
+++ app.js
@@ -600,7 +600,7 @@ app.get("/atualizartablefretes", async (req, res) => {
   var scroll_id_x = [""];
   var product_ids = [];
   const url1 = `https://api.mercadolibre.com/users/${SELLER_ID}/items/search`;
-  array_jsons_frete_apelado = [];
+  array_jsons_frete_apelado = []; //atribuindo o valor como vazio pra "reiniciar" a variável qdo o usuário rodar mais de 1x na mesma sessão
   try {
     const result = await axios.get(url1, {
       headers: `Authorization: Bearer ${fake_meli_token}`,
@@ -664,15 +664,14 @@ app.get("/atualizartablefretes", async (req, res) => {
           params: [JSON.stringify(array_jsons_frete_apelado)],
           nome: "Frete: inserir",
         });
-        console.log(`Frete: Inserção parcial dos fretes_apelados`);
-        // console.log(inserir);
+        console.log(`Frete: Inserção parcial dos fretes_apelados ${inserir}`);
         const ultima_data_atualizacao = await minha_query({
           texto:
             "SELECT id, data_atualizacao FROM public.fretes WHERE data_atualizacao IN" +
             "(SELECT data_atualizacao FROM public.fretes GROUP BY data_atualizacao LIMIT 1)",
           nome: "Frete: Puxar ultima data_atualizacao",
         });
-        console.log(ultima_data_atualizacao);
+        // console.log(ultima_data_atualizacao);
         res.render("home", {
           url_api: `https://api.mercadolibre.com/users/${SELLER_ID}/shipping_options/free?item_id=`,
           resultado_api:
